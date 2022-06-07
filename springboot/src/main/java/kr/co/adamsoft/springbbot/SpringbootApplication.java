738654d8a7098255f20bf7a661cbe742d391f4bd
itggangpae <ggangpae1@gmail.com>
2022-06-07 16:43:24 +0900

    2022_0607

---
+++ springboot/src/main/java/kr/co/adamsoft/springbbot/SpringbootApplication.java
@@ -0,0 +1,13 @@
+package kr.co.adamsoft.springbbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootApplication.class, args);
+	}
+
+}
